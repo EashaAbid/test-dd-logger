c3698dd9788f9c14e4609a74e777eed396c52571
Easha <easha.abid97@gmail.com>
2024-07-05 13:02:43 +0500

    add server

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const { makeHttpRequest } = require("./callApi");
+const express = require("express");
 
-setInterval(makeHttpRequest, 5000);
-process.stdin.resume();
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+  setInterval(makeHttpRequest, 5000);
+  //   process.stdin.resume();
+});
