7fe9ed560c218bceb5b93ed1f99ef3e44bb709f1
Easha <easha.abid97@gmail.com>
2024-07-05 12:24:29 +0500

    remove api call

--- callApi.js
+++ callApi.js
@@ -3,19 +3,11 @@ const logger = require("./logger");
 
 async function makeHttpRequest() {
   try {
-    const response = await axios.get("https://api.digitalocean.com/v2");
-
-    if (response.status !== 200) {
-      throw new Error(
-        `HTTP request to DigitalOcean failed with status code ${response.status}`
-      );
-    }
-
-    logger.log(`HTTP request successful: ${response.data.message}`);
-    console.log("Response:", response.data.message);
+    console.log("App Running:", Date());
+    logger.log("App Running:", Date());
   } catch (error) {
-    logger.log(error.message);
     console.error("Error:", error.message);
+    logger.log(error.message);
   }
 }
 
