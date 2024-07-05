71907287680186fd02863cf445bf20ba7273db97
Easha <easha.abid97@gmail.com>
2024-07-05 12:38:50 +0500

    rm file log

--- callApi.js
+++ callApi.js
@@ -4,10 +4,10 @@ const logger = require("./logger");
 async function makeHttpRequest() {
   try {
     console.log("App Running:", Date());
-    logger.log("App Running:", Date());
+    // logger.log("App Running:", Date());
   } catch (error) {
     console.error("Error:", error.message);
-    logger.log(error.message);
+    // logger.log(error.message);
   }
 }
 
